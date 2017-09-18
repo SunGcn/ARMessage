dd0cab081b01a9a8a92a9c421ef3c22fd42d1e6f
SunGcn <nixiaoqilaihenmeiwei@gmail.com>
2017-09-18 23:38:54 +0800

    add plane message

--- ARMessage/ViewController.swift
+++ ARMessage/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 import SceneKit
 import ARKit
 
+extension UIImage {
+    func drawTextInImage(text:String)->UIImage {
+        //开启图片上下文
+        UIGraphicsBeginImageContext(self.size)
+        //图形重绘
+        self.draw(in: CGRect(x:0,y:0,width:self.size.width,height:self.size.height))
+        //水印文字属性
+        let att = [NSAttributedStringKey.foregroundColor:UIColor.blue,NSAttributedStringKey.font:UIFont.systemFont(ofSize: 50),NSAttributedStringKey.backgroundColor:UIColor.white]
+        //水印文字大小
+        let text = NSString(string: text)
+        let size =  text.size(withAttributes: att)
+        //绘制文字
+        text.draw(in: CGRect(x:40,y:60, width:size.width, height:size.height), withAttributes: att)
+        //从当前上下文获取图片
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        //关闭上下文
+        UIGraphicsEndImageContext()
+        
+        return image!
+        
+    }
+}
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
@@ -30,23 +53,55 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.scene = scene*/
         
         
+        //生成带文字图片
+        let letter:String = "QT真的傻逼啊 安装这么麻烦"
+        // 取第一个字符(测试了,太长了的话,效果并不好)
+        //let letter = (text as NSString).substring(to: 1)
+        let sise = CGSize(width: 400, height: 50)
+        let rect = CGRect(origin: CGPoint.zero, size: sise)
+        // 开启上下文
+        UIGraphicsBeginImageContext(sise)
+        // 拿到上下文
+        let ctx = UIGraphicsGetCurrentContext()
+       
+        // 取较小的边
+        //let minSide = min(400.0, 50.0)
+        // 设置填充颜色
+        ctx?.setFillColor(UIColor(displayP3Red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 0.75).cgColor)
+        // 填充绘制
+        ctx?.fill(rect)
+        let attr = [ NSAttributedStringKey.foregroundColor : UIColor(displayP3Red: 59/255.0, green: 151/255.0, blue: 247/255.0, alpha: 1), NSAttributedStringKey.font : UIFont.systemFont(ofSize: 30)]
+        // 写入文字
+        (letter as NSString).draw(at: CGPoint(x: 50, y: 10), withAttributes: attr)
+        // 得到图片
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 关闭上下文
+        UIGraphicsEndImageContext()
+        
         
         
         // 存放所有 3D 几何体的容器
         let scene = SCNScene()
         // 想要绘制的 3D 立方体
-        let boxGeometry = SCNBox(width: 10, height: 1.2, length: 10, chamferRadius: 0)
+        let boxGeometry = SCNBox(width: 16, height: 2, length: 0, chamferRadius: 0)
         // 将几何体包装为 node 以便添加到 scene
         let boxNode = SCNNode(geometry: boxGeometry)
         boxNode.position = SCNVector3Make(0, 0, -30)
         //boxNode.rotation = SCNVector4Make(0.1,0.1,0.1,.pi)
-        
         let material = SCNMaterial()
-        material.diffuse.contents = UIImage(named: "text1")
-        boxNode.geometry?.materials  = [material, material, material, material, material, material]
-        boxNode.opacity = 0.5
+        material.diffuse.contents = image//UIImage(named: "text1")
+        let white = SCNMaterial()
+        white.diffuse.contents = UIImage(named: "white")
+        //let white = UIImage(named: "white")
+        boxNode.geometry?.materials  = [material, white, white, white, white, white]
+        boxNode.opacity = 1
         //boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
         
+        let planeGeometry = SCNPlane(width: 16,height: 2)
+        planeGeometry.cornerRadius = 0.2
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.position = SCNVector3Make(0, 0, -40)
         
         
         
